45d432a200c3d17f310c10f37f4c2864cb1305fc
LesleyPark <lesparkley@gmail.com>
2018-09-08 17:20:06 -0700

    Added contact to Navbar, font changes

---
+++ client/src/components/About.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+// import styles from '../stylesheets/About.css';
+
+class About extends React.Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
+
+  render () {
+    return (
+      <div>
+        Lesley Park is a photographer who has been shooting for the last 
+      </div>
+    );
+  }
+}
+
+export default About;
--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,40 +1,14 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import Navbar from './components/Navbar.jsx';
-// import Gallery from './components/Gallery.jsx';
-
-// class App extends React.Component {
-//   constructor() {
-//     super();
-//     this.state = {};
-//   }
-
-//   render () {
-//     return (
-//       <div>
-//         <div className="title"><h1>L E S L E Y&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;P A R K</h1>
-//           <div className="italics">
-//             <h5>Photographer</h5>
-//           </div>
-//           <hr />
-//           <Navbar />
-//         </div>
-//           <Gallery />
-//       </div>
-//     );
-//   }
-// }
-
-// ReactDOM.render(<App />, document.getElementById('app'));
-
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Navbar from './components/Navbar.jsx';
+import About from './components/About.jsx'
 
 class App extends React.Component {
   constructor() {
     super();
-    this.state = {};
+    this.state = {
+      aboutIsOpen: true,
+    };
   }
 
   render () {
@@ -46,6 +20,7 @@ class App extends React.Component {
           </div>
           <hr />
           <Navbar />
+          {this.state.aboutIsOpen ? <About /> : null}
         </div>
       </div>
     );
